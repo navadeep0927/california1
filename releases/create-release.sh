111c87f05e1343ee6e3814e5d6710a55d44236cf
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2018-06-26 23:12:22 -0700

    created branch count script for California repository

---
+++ releases/create-release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Entering the california repository"
+echo "finding all the branches"
+branch_count=`git branch|wc -l`
+echo "There are $branch_count branches in the california repository"
