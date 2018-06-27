38aa7b0bd794694d946d7278ab1d52bb64844f5d
Surya Vallabhaneni <surya.vallabhaneni2@gmail.com>
2018-06-26 23:33:18 -0700

    new branch creation

--- myshellscript.sh
+++ myshellscript.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo "This is my first program in GIT"
+
+echo "creating new branch"
+
+git checkout -b "new-branch"
+
+git push --set-upstream origin new-branch
+
+if [ $? -eq 0 ];then
+  echo "branch creation successful"
+fi
