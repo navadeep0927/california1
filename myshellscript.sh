fe13ac5724c5ca47f05cd3e6110c49b33ea378c2
[navadeep0927] <[navadeep0927@gmail.com]>
2018-06-27 01:42:41 -0500

    ljnlj

--- myshellscript.sh
+++ myshellscript.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-echo "This is my first script in GIT"
-
-echo "creating new branch"
-
-git checkout -b "new-branch"
-
-git push --set-upstream origin new-branch
-
-if [ $? -eq 0 ];then
-  echo "branch creation successful"
-fi
+echo "This is my first program in GIT"
+ls -ltr
